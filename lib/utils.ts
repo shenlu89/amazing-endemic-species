163458f7d41fd174b1809593be045a114ddeecc1
shenlu89 <shenlu89dev@gmail.com>
2024-05-07 00:04:55 +0800

    feat: add utils

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+const random = (min: number, max: number) => {
+  return Math.round(Math.random() * (max - min + 1)) + min;
+};
+
+const objToMap = (object: any) => new Map(Object.entries(object));
+
+export {random, objToMap};
