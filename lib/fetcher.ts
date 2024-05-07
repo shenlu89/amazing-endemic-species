32dd714c3734212bd4f69eb50c86311e2663b34e
shenlu89 <shenlu89dev@gmail.com>
2024-05-07 19:11:41 +0800

    feat: add fetcher

---
+++ lib/fetcher.ts
@@ -0,0 +1,7 @@
+export default async function fetcher<JSON = any>(
+  url: RequestInfo,
+  options?: RequestInit
+): Promise<JSON> {
+  const res = await fetch(url, options);
+  return res.json();
+}
