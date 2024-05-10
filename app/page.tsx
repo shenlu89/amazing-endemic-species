1ed51877e89f78ef58f5bd7095bcfae1e926d842
shenlu89 <shenlu89dev@gmail.com>
2024-05-11 01:52:03 +0800

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,279 @@
+"use client";
+import { geoOrthographic, interpolate, geoCentroid, geoPath, select } from "d3";
+import { feature } from "topojson-client";
+import { useEffect, useRef, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import fetcher from "@/lib/fetcher";
+import useSWR, { useSWRConfig } from "swr";
+import { random } from "@/lib/utils";
 import Image from "next/image";
+import getUnicodeFlagIcon from "country-flag-icons/unicode";
+import {
+  COUNTRY_NAME,
+  CONSERVATION_STATUS,
+  WIKI_URI,
+  IUCN_RED_LIST_URI,
+  ISO_MAP,
+  WORLD_110M,
+} from "@/lib/constant";
+import Link from "next/link";
+import Metrics from "@/components/metrics";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+  const [isMounted, setMounted] = useState(false);
+  const [species, setSpecies] = useState({
+    image: "",
+    name: "",
+    status: "",
+    country: "",
+    commonName: "",
+  });
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+  };
+  const svgRef = useRef<any>(null);
+  const width = 540;
+  const height = 540;
+
+  const { mutate } = useSWRConfig();
+  const { data, error, isLoading }: any = useSWR(WORLD_110M, fetcher, {
+    revalidateOnMount: true,
+  });
+
+  let {
+    data: _species,
+    isLoading: _loading,
+    error: _error,
+  }: any = useSWR("/api/v1/random", fetcher);
+
+  const titeRef = useRef<any>(null);
+
+  const handleAnimationEnd = useDebouncedCallback(() => {
+    if (titeRef.current) {
+      titeRef.current.textContent =
+        "Mapping amazing endemic species all over the world";
+    }
+  }, 1000);
+
+  const indexOfCountries = (specie: any, countries: any) => {
+    const code = ISO_MAP.get(specie.code);
+    if (code === 156) {
+      return [30, 163];
+    }
+    for (let i = 0, len = countries.length; i < len; i++) {
+      if (countries[i].id === code) {
+        return [i];
+      }
+    }
+    return [0];
+  };
+
+  const projection = geoOrthographic()
+    .scale(height / 2)
+    .translate([width / 2, height / 2])
+    .clipAngle(180)
+    .rotate([0, 0, 23.44]);
+  const path = geoPath().projection(projection);
+
+  useEffect(() => {
+    if (!data || isLoading || _loading) return;
+
+    const { features: countries }: any = feature(data, data.objects.countries);
+    const earth = select(svgRef.current);
+    earth
+      .selectAll()
+      .data(countries)
+      .enter()
+      .append("path")
+      .transition()
+      .delay((_, i) => i * 10)
+      .attr("d", (d: any) => path(d))
+      .transition()
+      .delay(300)
+      .attr("class", "back");
+
+    earth
+      .append("circle")
+      .style("fill", "#f8fafc")
+      .style("opacity", 0.7)
+      .attr("cx", width / 2)
+      .attr("cy", height / 2)
+      .attr("r", projection.scale());
+
+    projection.clipAngle(90);
+
+    earth
+      .selectAll()
+      .data(countries)
+      .enter()
+      .append("path")
+      .transition()
+      .delay((_, i) => i * 10)
+      .attr("d", (d: any) => path(d))
+      .transition()
+      .delay(300)
+      .attr("class", "front")
+      .transition()
+      .delay(3500)
+      .on("end", step);
+
+    async function step() {
+      const index = indexOfCountries(_species, countries);
+
+      earth
+        .selectAll(".front")
+        .transition()
+        .style("fill", (_, i) => (index.includes(i) ? "#dc2626" : "#64748b"));
+
+      earth
+        .transition()
+        .call(async () => {
+          setSpecies({
+            image: _species.id,
+            name: _species.name,
+            status: _species.status,
+            country: _species.code,
+            commonName: _species.common_name,
+          });
+          setIsLoaded(false);
+          _species = await mutate("/api/v1/random");
+        })
+        .delay(100)
+        .duration(1000)
+        .tween("rotate", () => {
+          const point = geoCentroid(countries[index[0]]);
+          const rotate: any = interpolate(projection.rotate(), [
+            -point[0],
+            -point[1],
+            23.44,
+          ]);
+          return (t) => {
+            projection.rotate(rotate(t)).clipAngle(180);
+            earth.selectAll(".back").attr("d", (d: any) => path(d));
+            projection.rotate(rotate(t)).clipAngle(90);
+            earth.selectAll(".front").attr("d", (d: any) => path(d));
+          };
+        })
+        .transition()
+        .delay(3500)
+        .on("end", step);
+    }
+    setMounted(true);
+    return () => {
+      earth.interrupt();
+    };
+  }, [data, error, isLoading, _loading, mutate]);
+  if (error) return <div>Fail to load!</div>;
+  if (isLoading && !isMounted)
+    return (
+      <div className="relative h-full">
+        <div className="loading">
+          {new Array(4).fill(null).map((_, index) => (
+            <div key={index} className="paw">
+              <svg>
+                <use xlinkHref="#paw" />
+              </svg>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center items-center h-full text-lg">
+          Loading...
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    );
+  return (
+    <div className="flex justify-center flex-1">
+      <h2
+        ref={titeRef}
+        className="text-xl font-bold text-center fixed px-4"
+        onAnimationEnd={handleAnimationEnd}
+      >
+        {Array.from("Mapping amazing endemic species all over the world").map(
+          (char, index) => (
+            <div
+              style={{
+                animationDelay: `${index * 15}ms`,
+                transform: `translate3d(${random(-300, 300) | 0}px, ${
+                  random(-300, 300) | 0
+                }px, ${random(-300, 300) | 0}px)`,
+              }}
+              className={`text-fly-in transform-gpu ${
+                char === " " ? "inline" : "inline-block"
+              }`}
+              key={index}
+            >
+              {char}
+            </div>
+          )
+        )}
+      </h2>
+      <Metrics />
+      <div className="flex flex-col justify-center items-center space-y-2 fixed md:mt-24 mt-[6.5rem]">
+        <div className="relative">
+          <Link
+            href={WIKI_URI + species.name.replace(/\s/g, "_")}
+            target="_blank"
+          >
+            <Image
+              src={`/images/${species.image}.jpg`}
+              width={130}
+              height={130}
+              alt=""
+              className={`rounded-full shadow ${
+                species.image || "opacity-0"
+              }  ${isLoaded || "blur-sm"}`}
+              onLoad={handleImageLoad}
+              priority
+            />
+          </Link>
+          {!_loading && (
+            <Link
+              href={WIKI_URI + CONSERVATION_STATUS.get(species.status)}
+              target="_blank"
+              className={`flex absolute top-0 right-0 text-sm items-center ${species.status} justify-center rounded-full w-8 h-8 font-bold`}
+            >
+              {species.status}
+            </Link>
+          )}
+        </div>
+        {!_loading || !species.name ? (
+          <div className="flex flex-col items-center space-y-1">
+            <Link
+              href={IUCN_RED_LIST_URI + species.name.replace(/\s/g, "%20")}
+              target="_blank"
+              className="font-bold text-lg hover:text-red-600"
+            >
+              {species.commonName}
+            </Link>
+            <Link
+              href={IUCN_RED_LIST_URI + species.name.replace(/\s/g, "%20")}
+              target="_blank"
+              className="hover:text-red-600"
+            >
+              {species.name}
+            </Link>
+            <Link
+              href={WIKI_URI + COUNTRY_NAME.get(species.country)}
+              target="_blank"
+              className="hover:text-red-600"
+            >{`${species.country && getUnicodeFlagIcon(species.country)} ${
+              species.country && COUNTRY_NAME.get(species.country)
+            }`}</Link>
+          </div>
+        ) : (
+          <div className="text-lg p-2">Loading...</div>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <svg
+        className="fixed earth"
+        preserveAspectRatio="xMidYMin meet"
+        viewBox={`0 0 ${width} ${height}`}
+        ref={svgRef}
+        width={width}
+        height={height}
+      ></svg>
+    </div>
   );
 }
