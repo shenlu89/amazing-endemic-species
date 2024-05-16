4b4e964731d7717905667479236cfcda1d16a82d
shenlu89 <shenlu89dev@gmail.com>
2024-05-16 08:16:19 +0800

    refactor: remove POST request for views

--- app/api/views/route.ts
+++ app/api/views/route.ts
@@ -1,34 +1,19 @@
-import {NextResponse} from 'next/server';
-import {Redis} from '@upstash/redis';
+import { NextResponse } from "next/server";
+import { Redis } from "@upstash/redis";
 
 const redis = Redis.fromEnv();
 
 export async function GET(_request: Request, name: string) {
   try {
-    const views = await redis.get(name);
-
-    return NextResponse.json(
-      {
-        total: views?.toString()
-      },
-      {status: 200}
-    );
-  } catch (e: any) {
-    return NextResponse.json({message: e.message}, {status: 500});
-  }
-}
-
-export async function POST(_request: Request, name: string) {
-  try {
     const views = await redis.incr(name);
 
     return NextResponse.json(
       {
-        total: views?.toString()
+        total: views?.toString(),
       },
-      {status: 200}
+      { status: 200 }
     );
   } catch (e: any) {
-    return NextResponse.json({message: e.message}, {status: 500});
+    return NextResponse.json({ message: e.message }, { status: 500 });
   }
 }
