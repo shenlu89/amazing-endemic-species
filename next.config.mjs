10aa3608253511aadbf4a69d530b1898cd33d1bd
shenlu89 <shenlu89dev@gmail.com>
2024-05-16 08:42:03 +0800

    build: add images fields in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: process.env.NODE_ENV === 'development' ? [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/images/**',
+      }
+    ] : [{
+      protocol: 'https',
+      hostname: 'aes.shenlu.me',
+      pathname: '/images/**',
+    }],
+  },
+  reactStrictMode: false,
+};
 
 export default nextConfig;
