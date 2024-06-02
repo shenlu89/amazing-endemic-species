eb62a7510a0fe012a4154e5253b66cf7694e3f13
shenlu89 <shenlu89dev@gmail.com>
2024-06-02 14:09:21 +0800

    fix: add missing link

--- components/add-more-species.tsx
+++ components/add-more-species.tsx
@@ -5,7 +5,7 @@ export default function AddMoreSpecies({ src, alt }: any) {
   return (
     <Link
       className="md:flex hidden hover:opacity-75 text-sm text-white bg-red-600 py-2 px-3 items-center rounded-full space-x-2 font-bold"
-      href="https://github.com/shenlu89/amazing-endemic-species?tab=readme-ov-file#contributing-guidelines"
+      href="https://github.com/shenlu89/amazing-endemic-species/issues/new?assignees=shenlu89&labels=add-more-species&projects=&template=add_more_species.md&title=%5BAdd+More+Species%5D"
       target="_blank"
     >
       <FiPlusCircle className="w-5 h-5" />
