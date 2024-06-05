63771cec97ec7e3f5d1fc7acd77b37c54f179506
shenlu89 <shenlu89dev@gmail.com>
2024-06-06 02:34:58 +0800

    chore: move function out of GET request

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -3,30 +3,30 @@ import { or, like, asc, desc } from "drizzle-orm";
 import { db } from "@/db";
 import { aes } from "@/db/schema";
 
+const orderByColumn = (sortType: string, sortDirection: string) => {
+  let column;
+  switch (sortType) {
+    case "common_name":
+      column = aes.common_name;
+      break;
+    case "scientific_name":
+      column = aes.scientific_name;
+      break;
+    case "iso_code":
+      column = aes.iso_code;
+      break;
+    default:
+      column = aes.id;
+      break;
+  }
+  return sortDirection === "asc" ? asc(column) : desc(column);
+};
+
 export async function GET(request: Request) {
   const { searchParams }: any = new URL(request.url);
   const query = decodeURI(searchParams.get("q") || "");
   const sortType = decodeURI(searchParams.get("sortType") || "id");
   const sortDirection = decodeURI(searchParams.get("sortDirection") || "asc");
-
-  const orderByColumn = (sortType: string, sortDirection: string) => {
-    let column;
-    switch (sortType) {
-      case "common_name":
-        column = aes.common_name;
-        break;
-      case "scientific_name":
-        column = aes.scientific_name;
-        break;
-      case "iso_code":
-        column = aes.iso_code;
-        break;
-      default:
-        column = aes.id;
-        break;
-    }
-    return sortDirection === "asc" ? asc(column) : desc(column);
-  };
 
   try {
     const species = await db
