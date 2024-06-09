add78284163d10b9ad036380abe5c9de11d662fd
shenlu89 <shenlu89dev@gmail.com>
2024-06-09 08:24:44 +0800

    fix: No Access-Control-Allow-Origin header is present on the requested resource (#13)

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers () {
+    return [
+      {
+        source: "/api/v1/:slug",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Set your origin
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: process.env.NODE_ENV === 'development' ? [
       {
